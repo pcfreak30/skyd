NebulousLabs/Sia

replace bolt with database interface

modules/consensus/changelog.go
@@ -22,8 +22,7 @@ import (
 	"github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
+	"github.com/NebulousLabs/Sia/modules/consensus/database"
 )
 
 var (
@@ -53,7 +52,7 @@ type (
 )
 
 // appendChangeLog adds a new change entry to the change log.
-func appendChangeLog(tx *bolt.Tx, ce changeEntry) error {
+func appendChangeLog(tx database.Tx, ce changeEntry) error {
 	// Insert the change entry.
 	cl := tx.Bucket(ChangeLog)
 	ceid := ce.ID()
@@ -94,7 +93,7 @@ func appendChangeLog(tx *bolt.Tx, ce changeEntry) error {
 
 // getEntry returns the change entry with a given id, using a bool to indicate
 // existence.
-func getEntry(tx *bolt.Tx, id modules.ConsensusChangeID) (ce changeEntry, exists bool) {
+func getEntry(tx database.Tx, id modules.ConsensusChangeID) (ce changeEntry, exists bool) {
 	var cn changeNode
 	cl := tx.Bucket(ChangeLog)
 	changeNodeBytes := cl.Get(id[:])
@@ -114,7 +113,7 @@ func (ce *changeEntry) ID() modules.ConsensusChangeID {
 }
 
 // NextEntry returns the entry after the current entry.
-func (ce *changeEntry) NextEntry(tx *bolt.Tx) (nextEntry changeEntry, exists bool) {
+func (ce *changeEntry) NextEntry(tx database.Tx) (nextEntry changeEntry, exists bool) {
 	// Get the change node associated with the provided change entry.
 	ceid := ce.ID()
 	var cn changeNode
@@ -129,7 +128,7 @@ func (ce *changeEntry) NextEntry(tx *bolt.Tx) (nextEntry changeEntry, exists boo
 }
 
 // createChangeLog assumes that no change log exists and creates a new one.
-func (cs *ConsensusSet) createChangeLog(tx *bolt.Tx) error {
+func (cs *ConsensusSet) createChangeLog(tx database.Tx) error {
 	// Create the changelog bucket.
 	cl, err := tx.CreateBucket(ChangeLog)
 	if err != nil {

modules/consensus/diffs_test.go
@@ -5,8 +5,7 @@ import (
 
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
+	"github.com/NebulousLabs/Sia/modules/consensus/database"
 )
 
 // TestCommitDelayedSiacoinOutputDiffBadMaturity commits a delayed siacoin
@@ -40,7 +39,7 @@ func TestCommitDelayedSiacoinOutputDiffBadMaturity(t *testing.T) {
 		SiacoinOutput:  dsco,
 		MaturityHeight: maturityHeight,
 	}
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitDelayedSiacoinOutputDiff(tx, dscod, modules.DiffApply)
 		return nil
 	})
@@ -58,7 +57,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 	}
 	defer cst.Close()
 	pb := cst.cs.dbCurrentProcessedBlock()
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitDiffSet(tx, pb, modules.DiffRevert) // pull the block node out of the consensus set.
 		return nil
 	})
@@ -100,7 +99,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 		MaturityHeight: cst.cs.dbBlockHeight() + types.MaturityDelay,
 	}
 	var siafundPool types.Currency
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		siafundPool = getSiafundPool(tx)
 		return nil
 	})
@@ -120,11 +119,11 @@ func TestCommitNodeDiffs(t *testing.T) {
 	pb.SiafundOutputDiffs = append(pb.SiafundOutputDiffs, sfod1)
 	pb.DelayedSiacoinOutputDiffs = append(pb.DelayedSiacoinOutputDiffs, dscod)
 	pb.SiafundPoolDiffs = append(pb.SiafundPoolDiffs, sfpd)
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		createUpcomingDelayedOutputMaps(tx, pb, modules.DiffApply)
 		return nil
 	})
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitNodeDiffs(tx, pb, modules.DiffApply)
 		return nil
 	})
@@ -140,7 +139,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 	if exists {
 		t.Error("intradependent outputs not treated correctly")
 	}
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitNodeDiffs(tx, pb, modules.DiffRevert)
 		return nil
 	})
@@ -573,7 +572,7 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 		t.Fatal(err)
 	}
 	pb := cst.cs.currentProcessedBlock()
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		return commitDiffSet(tx, pb, modules.DiffRevert)
 	})
 	if err != nil {
@@ -593,19 +592,19 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 		}
 
 		// Trigger a panic by deleting a map with outputs in it during revert.
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return createUpcomingDelayedOutputMaps(tx, pb, modules.DiffApply)
 		})
 		if err != nil {
 			t.Fatal(err)
 		}
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return commitNodeDiffs(tx, pb, modules.DiffApply)
 		})
 		if err != nil {
 			t.Fatal(err)
 		}
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return deleteObsoleteDelayedOutputMaps(tx, pb, modules.DiffRevert)
 		})
 		if err != nil {
@@ -614,7 +613,7 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 	}()
 
 	// Trigger a panic by deleting a map with outputs in it during apply.
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		return deleteObsoleteDelayedOutputMaps(tx, pb, modules.DiffApply)
 	})
 	if err != nil {

modules/consensus/fork_helpers_test.go
@@ -1,13 +1,11 @@
 package consensus
 
-import (
-	"github.com/coreos/bbolt"
-)
+import "github.com/NebulousLabs/Sia/modules/consensus/database"
 
 // dbBacktrackToCurrentPath is a convenience function to call
 // backtrackToCurrentPath without a bolt.Tx.
 func (cs *ConsensusSet) dbBacktrackToCurrentPath(pb *processedBlock) (pbs []*processedBlock) {
-	_ = cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cs.db.Update(func(tx database.Tx) error {
 		pbs = backtrackToCurrentPath(tx, pb)
 		return nil
 	})
@@ -17,7 +15,7 @@ func (cs *ConsensusSet) dbBacktrackToCurrentPath(pb *processedBlock) (pbs []*pro
 // dbRevertToNode is a convenience function to call revertToBlock without a
 // bolt.Tx.
 func (cs *ConsensusSet) dbRevertToNode(pb *processedBlock) (pbs []*processedBlock) {
-	_ = cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cs.db.Update(func(tx database.Tx) error {
 		pbs = cs.revertToBlock(tx, pb)
 		return nil
 	})
@@ -27,7 +25,7 @@ func (cs *ConsensusSet) dbRevertToNode(pb *processedBlock) (pbs []*processedBloc
 // dbForkBlockchain is a convenience function to call forkBlockchain without a
 // bolt.Tx.
 func (cs *ConsensusSet) dbForkBlockchain(pb *processedBlock) (revertedBlocks, appliedBlocks []*processedBlock, err error) {
-	updateErr := cs.db.Update(func(tx *bolt.Tx) error {
+	updateErr := cs.db.Update(func(tx database.Tx) error {
 		revertedBlocks, appliedBlocks, err = cs.forkBlockchain(tx, pb)
 		return nil
 	})

modules/consensus/synchronize_test.go
@@ -17,8 +17,7 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/modules/gateway"
 	"github.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
+	"github.com/NebulousLabs/Sia/modules/consensus/database"
 )
 
 // TestSynchronize tests that the consensus set can successfully synchronize
@@ -131,7 +130,7 @@ func TestBlockHistory(t *testing.T) {
 	}
 
 	var history [32]types.BlockID
-	_ = cst.cs.db.View(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.View(func(tx database.Tx) error {
 		history = blockHistory(tx)
 		return nil
 	})
@@ -597,7 +596,7 @@ func TestRPCSendBlockSendsOnlyNecessaryBlocks(t *testing.T) {
 		// Get blockIDs to send.
 		var history [32]types.BlockID
 		cs.mu.RLock()
-		err := cs.db.View(func(tx *bolt.Tx) error {
+		err := cs.db.View(func(tx database.Tx) error {
 			history = blockHistory(tx)
 			return nil
 		})

pcfreak30/skyd

modules/consensus/changelog.go
@@ -17,13 +17,12 @@ package consensus
 // the genesis block will call 'append' later on during initialization.
 
 import (
+	"gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/encoding"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
 )
 
 var (
@@ -53,7 +52,7 @@ type (
 )
 
 // appendChangeLog adds a new change entry to the change log.
-func appendChangeLog(tx *bolt.Tx, ce changeEntry) error {
+func appendChangeLog(tx database.Tx, ce changeEntry) error {
 	// Insert the change entry.
 	cl := tx.Bucket(ChangeLog)
 	ceid := ce.ID()
@@ -94,7 +93,7 @@ func appendChangeLog(tx *bolt.Tx, ce changeEntry) error {
 
 // getEntry returns the change entry with a given id, using a bool to indicate
 // existence.
-func getEntry(tx *bolt.Tx, id modules.ConsensusChangeID) (ce changeEntry, exists bool) {
+func getEntry(tx database.Tx, id modules.ConsensusChangeID) (ce changeEntry, exists bool) {
 	var cn changeNode
 	cl := tx.Bucket(ChangeLog)
 	changeNodeBytes := cl.Get(id[:])
@@ -114,7 +113,7 @@ func (ce *changeEntry) ID() modules.ConsensusChangeID {
 }
 
 // NextEntry returns the entry after the current entry.
-func (ce *changeEntry) NextEntry(tx *bolt.Tx) (nextEntry changeEntry, exists bool) {
+func (ce *changeEntry) NextEntry(tx database.Tx) (nextEntry changeEntry, exists bool) {
 	// Get the change node associated with the provided change entry.
 	ceid := ce.ID()
 	var cn changeNode
@@ -129,7 +128,7 @@ func (ce *changeEntry) NextEntry(tx *bolt.Tx) (nextEntry changeEntry, exists boo
 }
 
 // createChangeLog assumes that no change log exists and creates a new one.
-func (cs *ConsensusSet) createChangeLog(tx *bolt.Tx) error {
+func (cs *ConsensusSet) createChangeLog(tx database.Tx) error {
 	// Create the changelog bucket.
 	cl, err := tx.CreateBucket(ChangeLog)
 	if err != nil {

modules/consensus/diffs_test.go
@@ -3,10 +3,9 @@ package consensus
 import (
 	"testing"
 
+	"gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
 )
 
 // TestCommitDelayedSiacoinOutputDiffBadMaturity commits a delayed siacoin
@@ -40,7 +39,7 @@ func TestCommitDelayedSiacoinOutputDiffBadMaturity(t *testing.T) {
 		SiacoinOutput:  dsco,
 		MaturityHeight: maturityHeight,
 	}
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitDelayedSiacoinOutputDiff(tx, dscod, modules.DiffApply)
 		return nil
 	})
@@ -58,7 +57,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 	}
 	defer cst.Close()
 	pb := cst.cs.dbCurrentProcessedBlock()
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitDiffSet(tx, pb, modules.DiffRevert) // pull the block node out of the consensus set.
 		return nil
 	})
@@ -100,7 +99,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 		MaturityHeight: cst.cs.dbBlockHeight() + types.MaturityDelay,
 	}
 	var siafundPool types.Currency
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		siafundPool = getSiafundPool(tx)
 		return nil
 	})
@@ -120,11 +119,11 @@ func TestCommitNodeDiffs(t *testing.T) {
 	pb.SiafundOutputDiffs = append(pb.SiafundOutputDiffs, sfod1)
 	pb.DelayedSiacoinOutputDiffs = append(pb.DelayedSiacoinOutputDiffs, dscod)
 	pb.SiafundPoolDiffs = append(pb.SiafundPoolDiffs, sfpd)
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		createUpcomingDelayedOutputMaps(tx, pb, modules.DiffApply)
 		return nil
 	})
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitNodeDiffs(tx, pb, modules.DiffApply)
 		return nil
 	})
@@ -140,7 +139,7 @@ func TestCommitNodeDiffs(t *testing.T) {
 	if exists {
 		t.Error("intradependent outputs not treated correctly")
 	}
-	_ = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.Update(func(tx database.Tx) error {
 		commitNodeDiffs(tx, pb, modules.DiffRevert)
 		return nil
 	})
@@ -573,7 +572,7 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 		t.Fatal(err)
 	}
 	pb := cst.cs.currentProcessedBlock()
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		return commitDiffSet(tx, pb, modules.DiffRevert)
 	})
 	if err != nil {
@@ -593,19 +592,19 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 		}
 
 		// Trigger a panic by deleting a map with outputs in it during revert.
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return createUpcomingDelayedOutputMaps(tx, pb, modules.DiffApply)
 		})
 		if err != nil {
 			t.Fatal(err)
 		}
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return commitNodeDiffs(tx, pb, modules.DiffApply)
 		})
 		if err != nil {
 			t.Fatal(err)
 		}
-		err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+		err = cst.cs.db.Update(func(tx database.Tx) error {
 			return deleteObsoleteDelayedOutputMaps(tx, pb, modules.DiffRevert)
 		})
 		if err != nil {
@@ -614,7 +613,7 @@ func TestDeleteObsoleteDelayedOutputMapsSanity(t *testing.T) {
 	}()
 
 	// Trigger a panic by deleting a map with outputs in it during apply.
-	err = cst.cs.db.Update(func(tx *bolt.Tx) error {
+	err = cst.cs.db.Update(func(tx database.Tx) error {
 		return deleteObsoleteDelayedOutputMaps(tx, pb, modules.DiffApply)
 	})
 	if err != nil {

modules/consensus/fork_helpers_test.go
@@ -1,13 +1,11 @@
 package consensus
 
-import (
-	"github.com/coreos/bbolt"
-)
+import "gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 
 // dbBacktrackToCurrentPath is a convenience function to call
 // backtrackToCurrentPath without a bolt.Tx.
 func (cs *ConsensusSet) dbBacktrackToCurrentPath(pb *processedBlock) (pbs []*processedBlock) {
-	_ = cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cs.db.Update(func(tx database.Tx) error {
 		pbs = backtrackToCurrentPath(tx, pb)
 		return nil
 	})
@@ -17,7 +15,7 @@ func (cs *ConsensusSet) dbBacktrackToCurrentPath(pb *processedBlock) (pbs []*pro
 // dbRevertToNode is a convenience function to call revertToBlock without a
 // bolt.Tx.
 func (cs *ConsensusSet) dbRevertToNode(pb *processedBlock) (pbs []*processedBlock) {
-	_ = cs.db.Update(func(tx *bolt.Tx) error {
+	_ = cs.db.Update(func(tx database.Tx) error {
 		pbs = cs.revertToBlock(tx, pb)
 		return nil
 	})
@@ -27,7 +25,7 @@ func (cs *ConsensusSet) dbRevertToNode(pb *processedBlock) (pbs []*processedBloc
 // dbForkBlockchain is a convenience function to call forkBlockchain without a
 // bolt.Tx.
 func (cs *ConsensusSet) dbForkBlockchain(pb *processedBlock) (revertedBlocks, appliedBlocks []*processedBlock, err error) {
-	updateErr := cs.db.Update(func(tx *bolt.Tx) error {
+	updateErr := cs.db.Update(func(tx database.Tx) error {
 		revertedBlocks, appliedBlocks, err = cs.forkBlockchain(tx, pb)
 		return nil
 	})

modules/consensus/synchronize_test.go
@@ -11,14 +11,13 @@ import (
 	"testing"
 	"time"
 
+	"gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/encoding"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/gateway"
 	"gitlab.com/NebulousLabs/Sia/types"
-
-	"github.com/coreos/bbolt"
 )
 
 // TestSynchronize tests that the consensus set can successfully synchronize
@@ -131,7 +130,7 @@ func TestBlockHistory(t *testing.T) {
 	}
 
 	var history [32]types.BlockID
-	_ = cst.cs.db.View(func(tx *bolt.Tx) error {
+	_ = cst.cs.db.View(func(tx database.Tx) error {
 		history = blockHistory(tx)
 		return nil
 	})
@@ -597,7 +596,7 @@ func TestRPCSendBlockSendsOnlyNecessaryBlocks(t *testing.T) {
 		// Get blockIDs to send.
 		var history [32]types.BlockID
 		cs.mu.RLock()
-		err := cs.db.View(func(tx *bolt.Tx) error {
+		err := cs.db.View(func(tx database.Tx) error {
 			history = blockHistory(tx)
 			return nil
 		})