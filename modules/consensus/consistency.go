NebulousLabs/Sia

add ConsensusChecksum to Tx

modules/consensus/consistency.go
@@ -6,75 +6,22 @@ import (
 	"fmt"
 
 	"github.com/NebulousLabs/Sia/build"
-	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/modules/consensus/database"
 	"github.com/NebulousLabs/Sia/types"
 	"github.com/NebulousLabs/fastrand"
-	"github.com/NebulousLabs/Sia/modules/consensus/database"
 
 	"github.com/coreos/bbolt"
 )
 
 // manageErr handles an error detected by the consistency checks.
 func manageErr(tx database.Tx, err error) {
-	markInconsistency(tx)
+	tx.MarkInconsistent()
 	if build.DEBUG {
 		panic(err)
 	} else {
 		fmt.Println(err)
 	}
-}
-
-// consensusChecksum grabs a checksum of the consensus set by pushing all of
-// the elements in sorted order into a merkle tree and taking the root. All
-// consensus sets with the same current block should have identical consensus
-// checksums.
-func consensusChecksum(tx database.Tx) crypto.Hash {
-	// Create a checksum tree.
-	tree := crypto.NewTree()
-
-	// For all of the constant buckets, push every key and every value. Buckets
-	// are sorted in byte-order, therefore this operation is deterministic.
-	consensusSetBuckets := []*bolt.Bucket{
-		tx.Bucket(BlockPath),
-		tx.Bucket(SiacoinOutputs),
-		tx.Bucket(FileContracts),
-		tx.Bucket(SiafundOutputs),
-		tx.Bucket(SiafundPool),
-	}
-	for i := range consensusSetBuckets {
-		err := consensusSetBuckets[i].ForEach(func(k, v []byte) error {
-			tree.Push(k)
-			tree.Push(v)
-			return nil
-		})
-		if err != nil {
-			manageErr(tx, err)
-		}
-	}
-
-	// Iterate through all the buckets looking for buckets prefixed with
-	// prefixDSCO or prefixFCEX. Buckets are presented in byte-sorted order by
-	// name.
-	err := tx.ForEach(func(name []byte, b *bolt.Bucket) error {
-		// If the bucket is not a delayed siacoin output bucket or a file
-		// contract expiration bucket, skip.
-		if !bytes.HasPrefix(name, prefixDSCO) && !bytes.HasPrefix(name, prefixFCEX) {
-			return nil
-		}
-
-		// The bucket is a prefixed bucket - add all elements to the tree.
-		return b.ForEach(func(k, v []byte) error {
-			tree.Push(k)
-			tree.Push(v)
-			return nil
-		})
-	})
-	if err != nil {
-		manageErr(tx, err)
-	}
-
-	return tree.Root()
 }
 
 // checkSiacoinCount checks that the number of siacoins countable within the
@@ -299,14 +246,14 @@ func (cs *ConsensusSet) checkRevertApply(tx database.Tx) {
 	if err != nil {
 		manageErr(tx, err)
 	}
-	if consensusChecksum(tx) != parent.ConsensusChecksum {
+	if tx.ConsensusChecksum() != parent.ConsensusChecksum {
 		manageErr(tx, errors.New("consensus checksum mismatch after reverting"))
 	}
 	_, _, err = cs.forkBlockchain(tx, current)
 	if err != nil {
 		manageErr(tx, err)
 	}
-	if consensusChecksum(tx) != current.ConsensusChecksum {
+	if tx.ConsensusChecksum() != current.ConsensusChecksum {
 		manageErr(tx, errors.New("consensus checksum mismatch after re-applying"))
 	}
 }

modules/consensus/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/persist"
 	"github.com/coreos/bbolt"
 )
@@ -99,6 +100,16 @@ func Open(filename string) (DB, error) {
 				return err
 			}
 		}
+
+		// If consistency flag is not set, initialize it to false (no
+		// inconsistencies).
+		if tx.Bucket(consistency).Get(consistency) == nil {
+			err = tx.Bucket(consistency).Put(consistency, encoding.Marshal(false))
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
 	})
 	if err != nil {

modules/consensus/database/tx.go
@@ -1,6 +1,14 @@
 package database
 
-import "github.com/coreos/bbolt"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/NebulousLabs/Sia/build"
+	"github.com/NebulousLabs/Sia/crypto"
+	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/coreos/bbolt"
+)
 
 // A Tx is a database transaction.
 type Tx interface {
@@ -9,8 +17,84 @@ type Tx interface {
 	CreateBucketIfNotExists(name []byte) (*bolt.Bucket, error)
 	DeleteBucket(name []byte) error
 	ForEach(func([]byte, *bolt.Bucket) error) error
+
+	// ConsensusChecksum grabs a checksum of the consensus set by pushing all
+	// of the elements in sorted order into a Merkle tree and taking the root.
+	// All consensus sets with the same current block should have identical
+	// consensus checksums.
+	ConsensusChecksum() crypto.Hash
+
+	// MarkInconsistent marks the database as inconsistent.
+	MarkInconsistent()
 }
 
 type txWrapper struct {
 	*bolt.Tx
+}
+
+// manageErr handles an error detected by the consistency checks.
+func manageErr(tx Tx, err error) {
+	tx.MarkInconsistent()
+	if build.DEBUG {
+		panic(err)
+	} else {
+		fmt.Println(err)
+	}
+}
+
+// ConsensusChecksum implements the Tx interface.
+func (tx txWrapper) ConsensusChecksum() crypto.Hash {
+	// Create a checksum tree.
+	tree := crypto.NewTree()
+
+	// For all of the constant buckets, push every key and every value. Buckets
+	// are sorted in byte-order, therefore this operation is deterministic.
+	consensusSetBuckets := []*bolt.Bucket{
+		tx.Bucket(blockPath),
+		tx.Bucket(siacoinOutputs),
+		tx.Bucket(fileContracts),
+		tx.Bucket(siafundOutputs),
+		tx.Bucket(siafundPool),
+	}
+	for i := range consensusSetBuckets {
+		err := consensusSetBuckets[i].ForEach(func(k, v []byte) error {
+			tree.Push(k)
+			tree.Push(v)
+			return nil
+		})
+		if err != nil {
+			manageErr(tx, err)
+		}
+	}
+
+	// Iterate through all the buckets looking for buckets prefixed with
+	// prefixDSCO or prefixFCEX. Buckets are presented in byte-sorted order by
+	// name.
+	err := tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+		// If the bucket is not a delayed siacoin output bucket or a file
+		// contract expiration bucket, skip.
+		if !bytes.HasPrefix(name, prefixDSCO) && !bytes.HasPrefix(name, prefixFCEX) {
+			return nil
+		}
+
+		// The bucket is a prefixed bucket - add all elements to the tree.
+		return b.ForEach(func(k, v []byte) error {
+			tree.Push(k)
+			tree.Push(v)
+			return nil
+		})
+	})
+	if err != nil {
+		manageErr(tx, err)
+	}
+
+	return tree.Root()
+}
+
+// MarkInconsistent implements the Tx interface.
+func (tx txWrapper) MarkInconsistent() {
+	cerr := tx.Bucket(consistency).Put(consistency, encoding.Marshal(true))
+	if build.DEBUG && cerr != nil {
+		panic(cerr)
+	}
 }

pcfreak30/skyd

modules/consensus/consistency.go
@@ -5,75 +5,22 @@ import (
 	"errors"
 	"fmt"
 
-	"gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 	"github.com/coreos/bbolt"
 	"gitlab.com/NebulousLabs/Sia/build"
-	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/encoding"
+	"gitlab.com/NebulousLabs/Sia/modules/consensus/database"
 	"gitlab.com/NebulousLabs/Sia/types"
 	"gitlab.com/NebulousLabs/fastrand"
 )
 
 // manageErr handles an error detected by the consistency checks.
 func manageErr(tx database.Tx, err error) {
-	markInconsistency(tx)
+	tx.MarkInconsistent()
 	if build.DEBUG {
 		panic(err)
 	} else {
 		fmt.Println(err)
 	}
-}
-
-// consensusChecksum grabs a checksum of the consensus set by pushing all of
-// the elements in sorted order into a merkle tree and taking the root. All
-// consensus sets with the same current block should have identical consensus
-// checksums.
-func consensusChecksum(tx database.Tx) crypto.Hash {
-	// Create a checksum tree.
-	tree := crypto.NewTree()
-
-	// For all of the constant buckets, push every key and every value. Buckets
-	// are sorted in byte-order, therefore this operation is deterministic.
-	consensusSetBuckets := []*bolt.Bucket{
-		tx.Bucket(BlockPath),
-		tx.Bucket(SiacoinOutputs),
-		tx.Bucket(FileContracts),
-		tx.Bucket(SiafundOutputs),
-		tx.Bucket(SiafundPool),
-	}
-	for i := range consensusSetBuckets {
-		err := consensusSetBuckets[i].ForEach(func(k, v []byte) error {
-			tree.Push(k)
-			tree.Push(v)
-			return nil
-		})
-		if err != nil {
-			manageErr(tx, err)
-		}
-	}
-
-	// Iterate through all the buckets looking for buckets prefixed with
-	// prefixDSCO or prefixFCEX. Buckets are presented in byte-sorted order by
-	// name.
-	err := tx.ForEach(func(name []byte, b *bolt.Bucket) error {
-		// If the bucket is not a delayed siacoin output bucket or a file
-		// contract expiration bucket, skip.
-		if !bytes.HasPrefix(name, prefixDSCO) && !bytes.HasPrefix(name, prefixFCEX) {
-			return nil
-		}
-
-		// The bucket is a prefixed bucket - add all elements to the tree.
-		return b.ForEach(func(k, v []byte) error {
-			tree.Push(k)
-			tree.Push(v)
-			return nil
-		})
-	})
-	if err != nil {
-		manageErr(tx, err)
-	}
-
-	return tree.Root()
 }
 
 // checkSiacoinCount checks that the number of siacoins countable within the
@@ -298,14 +245,14 @@ func (cs *ConsensusSet) checkRevertApply(tx database.Tx) {
 	if err != nil {
 		manageErr(tx, err)
 	}
-	if consensusChecksum(tx) != parent.ConsensusChecksum {
+	if tx.ConsensusChecksum() != parent.ConsensusChecksum {
 		manageErr(tx, errors.New("consensus checksum mismatch after reverting"))
 	}
 	_, _, err = cs.forkBlockchain(tx, current)
 	if err != nil {
 		manageErr(tx, err)
 	}
-	if consensusChecksum(tx) != current.ConsensusChecksum {
+	if tx.ConsensusChecksum() != current.ConsensusChecksum {
 		manageErr(tx, errors.New("consensus checksum mismatch after re-applying"))
 	}
 }

modules/consensus/database/database.go
@@ -1,8 +1,9 @@
 package database
 
 import (
-	"gitlab.com/NebulousLabs/Sia/persist"
 	"github.com/coreos/bbolt"
+	"gitlab.com/NebulousLabs/Sia/encoding"
+	"gitlab.com/NebulousLabs/Sia/persist"
 )
 
 var (
@@ -99,6 +100,16 @@ func Open(filename string) (DB, error) {
 				return err
 			}
 		}
+
+		// If consistency flag is not set, initialize it to false (no
+		// inconsistencies).
+		if tx.Bucket(consistency).Get(consistency) == nil {
+			err = tx.Bucket(consistency).Put(consistency, encoding.Marshal(false))
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
 	})
 	if err != nil {

modules/consensus/database/tx.go
@@ -1,6 +1,14 @@
 package database
 
-import "github.com/coreos/bbolt"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/coreos/bbolt"
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/encoding"
+)
 
 // A Tx is a database transaction.
 type Tx interface {
@@ -9,8 +17,84 @@ type Tx interface {
 	CreateBucketIfNotExists(name []byte) (*bolt.Bucket, error)
 	DeleteBucket(name []byte) error
 	ForEach(func([]byte, *bolt.Bucket) error) error
+
+	// ConsensusChecksum grabs a checksum of the consensus set by pushing all
+	// of the elements in sorted order into a Merkle tree and taking the root.
+	// All consensus sets with the same current block should have identical
+	// consensus checksums.
+	ConsensusChecksum() crypto.Hash
+
+	// MarkInconsistent marks the database as inconsistent.
+	MarkInconsistent()
 }
 
 type txWrapper struct {
 	*bolt.Tx
+}
+
+// manageErr handles an error detected by the consistency checks.
+func manageErr(tx Tx, err error) {
+	tx.MarkInconsistent()
+	if build.DEBUG {
+		panic(err)
+	} else {
+		fmt.Println(err)
+	}
+}
+
+// ConsensusChecksum implements the Tx interface.
+func (tx txWrapper) ConsensusChecksum() crypto.Hash {
+	// Create a checksum tree.
+	tree := crypto.NewTree()
+
+	// For all of the constant buckets, push every key and every value. Buckets
+	// are sorted in byte-order, therefore this operation is deterministic.
+	consensusSetBuckets := []*bolt.Bucket{
+		tx.Bucket(blockPath),
+		tx.Bucket(siacoinOutputs),
+		tx.Bucket(fileContracts),
+		tx.Bucket(siafundOutputs),
+		tx.Bucket(siafundPool),
+	}
+	for i := range consensusSetBuckets {
+		err := consensusSetBuckets[i].ForEach(func(k, v []byte) error {
+			tree.Push(k)
+			tree.Push(v)
+			return nil
+		})
+		if err != nil {
+			manageErr(tx, err)
+		}
+	}
+
+	// Iterate through all the buckets looking for buckets prefixed with
+	// prefixDSCO or prefixFCEX. Buckets are presented in byte-sorted order by
+	// name.
+	err := tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+		// If the bucket is not a delayed siacoin output bucket or a file
+		// contract expiration bucket, skip.
+		if !bytes.HasPrefix(name, prefixDSCO) && !bytes.HasPrefix(name, prefixFCEX) {
+			return nil
+		}
+
+		// The bucket is a prefixed bucket - add all elements to the tree.
+		return b.ForEach(func(k, v []byte) error {
+			tree.Push(k)
+			tree.Push(v)
+			return nil
+		})
+	})
+	if err != nil {
+		manageErr(tx, err)
+	}
+
+	return tree.Root()
+}
+
+// MarkInconsistent implements the Tx interface.
+func (tx txWrapper) MarkInconsistent() {
+	cerr := tx.Bucket(consistency).Put(consistency, encoding.Marshal(true))
+	if build.DEBUG && cerr != nil {
+		panic(cerr)
+	}
 }