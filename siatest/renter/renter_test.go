ec581b97d082dd214e5a24da0e92b38c215cd7ac
lukechampine <luke.champine@gmail.com>
2019-07-02 15:55:16 -0400

    set proper allowance.Hosts in TestRemoteBackup

--- siatest/renter/renter_test.go
+++ siatest/renter/renter_test.go
@@ -4822,8 +4822,16 @@ func TestRemoteBackup(t *testing.T) {
 			t.Fatal(err)
 		}
 	}()
-	// Create a subdir in the renter's files folder.
+
+	// Set the renter's allowance to two hosts.
 	r := tg.Renters()[0]
+	allowance := siatest.DefaultAllowance
+	allowance.Hosts = 2
+	if err := r.RenterPostAllowance(allowance); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a subdir in the renter's files folder.
 	subDir, err := r.FilesDir().CreateDir("subDir")
 	if err != nil {
 		t.Fatal(err)
