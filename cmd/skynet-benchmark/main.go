afa549e5a1dba1ee98ac3d33f5f30663059d7140
PJ <peterjan.brone@gmail.com>
2020-05-06 11:49:17 +0200

    Undo changes to benchmarking tool

--- cmd/skynet-benchmark/main.go
+++ cmd/skynet-benchmark/main.go
@@ -104,26 +104,24 @@ func main() {
 	}
 	fmt.Println("64kb files are ready to go.")
 
-	if false { // TODO REMOVE ME
-		err = uploadFileSet(dir1mbPath, 982e3, 983040)
-		if err != nil {
-			fmt.Println("Unable to upload 1mb files:", err)
-			return
-		}
-		fmt.Println("1mb files are ready to go.")
-		err = uploadFileSet(dir4mbPath, 3931e3, 3932160)
-		if err != nil {
-			fmt.Println("Unable to upload 4mb files:", err)
-			return
-		}
-		fmt.Println("4mb files are ready to go.")
-		err = uploadFileSet(dir10mbPath, 10e6, 10e6)
-		if err != nil {
-			fmt.Println("Unable to upload 10mb files:", err)
-			return
-		}
-		fmt.Printf("10mb files are ready to go.\n\n")
+	err = uploadFileSet(dir1mbPath, 982e3, 983040)
+	if err != nil {
+		fmt.Println("Unable to upload 1mb files:", err)
+		return
 	}
+	fmt.Println("1mb files are ready to go.")
+	err = uploadFileSet(dir4mbPath, 3931e3, 3932160)
+	if err != nil {
+		fmt.Println("Unable to upload 4mb files:", err)
+		return
+	}
+	fmt.Println("4mb files are ready to go.")
+	err = uploadFileSet(dir10mbPath, 10e6, 10e6)
+	if err != nil {
+		fmt.Println("Unable to upload 10mb files:", err)
+		return
+	}
+	fmt.Printf("10mb files are ready to go.\n\n")
 
 	fmt.Printf("Beginning download testing. Each test is %v files\n\n", filesPerDir)
 
@@ -136,7 +134,6 @@ func main() {
 			fmt.Println("Unable to download all 64kb files:", err)
 		}
 		fmt.Printf("64kb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
-		continue // TODO REMOVE ME
 		downloadStart = time.Now()
 		err = downloadFileSet(dir1mbPath, threads)
 		if err != nil {
