088dddebafc63ef4b6b67dcf51720237e222c1ed
PJ <peterjan.brone@gmail.com>
2020-08-20 15:03:18 +0200

    Ensure the dependency implements the Resolver

--- modules/host/dependencies_test.go
+++ modules/host/dependencies_test.go
@@ -15,6 +15,11 @@ type (
 	}
 )
 
+// Resolver returns the dependency itself, which implements the LookupIP method.
+func (d *dependencyCustomLookupIP) Resolver() modules.Resolver {
+	return d
+}
+
 // LookupIP calls the custom lookupIP method the struct was created with.
 func (d *dependencyCustomLookupIP) LookupIP(host string) ([]net.IP, error) {
 	return d.lookupIP(host)
