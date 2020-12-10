8045cfbbe1411c6c1f80faaea5e8c9cdc4679d6a
Christopher Schinnerl <ch.schinnerl@gmail.com>
2020-12-10 16:26:44 +0100

    fix lint

--- modules/renter/registry_test.go
+++ modules/renter/registry_test.go
@@ -10,7 +10,7 @@ import (
 func TestReadResponseSet(t *testing.T) {
 	// Get a set and fill it up completely.
 	n := 10
-	c := make(chan *jobReadRegistryResponse, n)
+	c := make(chan *jobReadRegistryResponse)
 	set := newReadResponseSet(c, n)
 	go func() {
 		for i := 0; i < n; i++ {
@@ -51,6 +51,7 @@ func TestReadResponseSet(t *testing.T) {
 	}
 
 	// Create another set that is collected right away.
+	c = make(chan *jobReadRegistryResponse)
 	set = newReadResponseSet(c, n)
 	go func() {
 		for i := 0; i < n; i++ {
@@ -64,12 +65,32 @@ func TestReadResponseSet(t *testing.T) {
 		}
 	}
 
+	// Create another set that is collected halfway and then cancelled.
+	c = make(chan *jobReadRegistryResponse)
+	set = newReadResponseSet(c, n/2)
+	ctx, cancel = context.WithCancel(context.Background())
+	go func(cancel context.CancelFunc) {
+		for i := 0; i < n/2; i++ {
+			c <- &jobReadRegistryResponse{staticErr: fmt.Errorf("%v", i)}
+		}
+		cancel()
+	}(cancel)
+	resps = set.collect(ctx)
+	if len(resps) != n/2 {
+		t.Fatal("wrong number of resps", len(resps), n/2)
+	}
+	for i, resp := range resps {
+		if resp.staticErr.Error() != fmt.Sprint(i) {
+			t.Fatal("wrong error", resp.staticErr, fmt.Sprint(i))
+		}
+	}
+
 	// Collect a set without responses with a closed context.
 	set = newReadResponseSet(c, n)
 	ctx, cancel = context.WithCancel(context.Background())
 	go cancel()
 	resps = set.collect(ctx)
-	if resps != nil {
-		t.Fatal("resps should be nil")
+	if len(resps) != 0 {
+		t.Fatal("resps should be empty", resps)
 	}
 }
