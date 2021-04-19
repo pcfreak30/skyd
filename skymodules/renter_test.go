53e100b10d85fd0c45d8822aff370eb50a532406
PJ <peterjan.brone@gmail.com>
2021-04-19 16:27:00 +0200

    Fix spending breakdown

--- skymodules/renter_test.go
+++ skymodules/renter_test.go
@@ -318,3 +318,53 @@ func TestRenterPayoutsPreTax(t *testing.T) {
 		t.Fatal("Negative currency returned for host collateral", hostCollateral)
 	}
 }
+
+// TestContractSpendingBreakdown is a unit test that probes SpendingBreakdown
+func TestContractorSpending_SpendingBreakdown(t *testing.T) {
+	randCurrency := func() types.Currency {
+		return types.NewCurrency64(fastrand.Uint64n(1e3))
+	}
+
+	cs := &ContractorSpending{
+		ContractFees:        randCurrency(),
+		DownloadSpending:    randCurrency(),
+		FundAccountSpending: randCurrency(),
+		MaintenanceSpending: MaintenanceSpending{
+			AccountBalanceCost:   randCurrency(),
+			FundAccountCost:      randCurrency(),
+			UpdatePriceTableCost: randCurrency(),
+		},
+		StorageSpending:  randCurrency(),
+		TotalAllocated:   randCurrency(),
+		UploadSpending:   randCurrency(),
+		Unspent:          randCurrency(),
+		WithheldFunds:    randCurrency(),
+		PreviousSpending: randCurrency(),
+	}
+
+	totalSpent, unspentAllocated, unspentUnallocated := cs.SpendingBreakdown()
+
+	// compare total spent vs expected value
+	expectedTotalSpent := cs.ContractFees.Add(cs.UploadSpending).Add(cs.DownloadSpending).Add(cs.StorageSpending).Add(cs.FundAccountSpending).Add(cs.MaintenanceSpending.Sum())
+	if !expectedTotalSpent.Equals(totalSpent) {
+		t.Fatal("unexpected")
+	}
+
+	// compare unspentAllocated vs expected value
+	var expectedUnspentAllocated types.Currency
+	if cs.TotalAllocated.Cmp(expectedTotalSpent) >= 0 {
+		expectedUnspentAllocated = cs.TotalAllocated.Sub(expectedTotalSpent)
+	}
+	if !expectedUnspentAllocated.Equals(unspentAllocated) {
+		t.Fatal("unexpected")
+	}
+
+	// compare unspentUnallocated vs expected value
+	var expectedUnspentUnallocated types.Currency
+	if cs.Unspent.Cmp(expectedUnspentAllocated) >= 0 {
+		expectedUnspentUnallocated = cs.Unspent.Sub(expectedUnspentAllocated)
+	}
+	if !expectedUnspentUnallocated.Equals(unspentUnallocated) {
+		t.Fatal("unexpected")
+	}
+}
