7719eaacc1dd5a5bb681f6825bdf2a246fe1193c
Matthew Sevey <mjsevey@gmail.com>
2021-04-22 15:05:24 -0400

    Fix copy paste

--- siatest/skynet/consts.go
+++ siatest/skynet/consts.go
@@ -7,11 +7,11 @@ import (
 	"gitlab.com/SkynetLabs/skyd/siatest"
 )
 
-// skynetTestDir creates a temporary testing directory for a renter test. This
+// skynetTestDir creates a temporary testing directory for a skynet test. This
 // should only every be called once per test. Otherwise it will delete the
 // directory again.
 func skynetTestDir(testName string) string {
-	path := siatest.TestDir("renter", testName)
+	path := siatest.TestDir("skynet", testName)
 	if err := os.MkdirAll(path, persist.DefaultDiskPermissionsTest); err != nil {
 		panic(err)
 	}
