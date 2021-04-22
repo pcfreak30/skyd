7d652d46743a8536e411e5a479ee77afb788165e
Matthew Sevey <mjsevey@gmail.com>
2021-04-22 17:35:37 -0400

    Update comments

--- skymodules/consts.go
+++ skymodules/consts.go
@@ -7,8 +7,10 @@ const (
 	StackSize = 64e6 // 64MB
 )
 
+// skynetPayoutAddress is an address owned by Skynet Labs
 var skynetPayoutAddress = [32]byte{14, 56, 201, 152, 87, 64, 139, 125, 38, 4, 161, 206, 32, 198, 119, 108, 158, 66, 177, 5, 178, 222, 155, 12, 209, 231, 91, 170, 213, 236, 57, 197}
 
+// SkynetPayoutAddress is the address used for skynet related payouts
 func SkynetPayoutAddress() types.UnlockHash {
 	return skynetPayoutAddress
 }
