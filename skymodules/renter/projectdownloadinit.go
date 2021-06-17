8c06bc220cb7c5a8dc00c4b166e921e439e72273
Chris Schinnerl <chris@nebulous.tech>
2021-06-17 15:24:55 +0200

    consider maintenance

--- skymodules/renter/projectdownloadinit.go
+++ skymodules/renter/projectdownloadinit.go
@@ -176,6 +176,15 @@ func (pdc *projectDownloadChunk) initialWorkerHeap(unresolvedWorkers []*pcwsUnre
 			continue
 		}
 
+		// Verify whether the worker is doing some sort of blocking
+		// maintenance which might delay scheduling jobs.
+		if w.staticSuspectRevisionMismatch() {
+			continue
+		}
+		if w.managedNeedsToSyncAccountBalanceToHost() {
+			continue
+		}
+
 		// Fetch the resolveTime, which is the time until the HS job is expected
 		// to resolve. If that time is in the past, add a penalty that assumes
 		// that there is some unusual circumstance preventing the worker from
@@ -237,6 +246,15 @@ func (pdc *projectDownloadChunk) initialWorkerHeap(unresolvedWorkers []*pcwsUnre
 			// perform async work, or if the read queue is on a cooldown.
 			jrq := w.staticJobReadQueue
 			if !w.managedAsyncReady() || jrq.callOnCooldown() {
+				continue
+			}
+
+			// Verify whether the worker is doing some sort of blocking
+			// maintenance which might delay scheduling jobs.
+			if w.staticSuspectRevisionMismatch() {
+				continue
+			}
+			if w.managedNeedsToSyncAccountBalanceToHost() {
 				continue
 			}
 
