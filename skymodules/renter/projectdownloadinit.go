ccb6c4cc087b8098e51a71f8ac27de702da1fc82
Chris Schinnerl <chris@nebulous.tech>
2021-06-28 10:33:03 +0200

    reduce max wait to 10ms

--- skymodules/renter/projectdownloadinit.go
+++ skymodules/renter/projectdownloadinit.go
@@ -92,7 +92,7 @@ import (
 // maxWaitUnresolvedWorkerUpdate defines the amount of time we want to wait for
 // unresolved workers to become resolved when trying to create the initial
 // worker set.
-const maxWaitUnresolvedWorkerUpdate = 30 * time.Millisecond
+const maxWaitUnresolvedWorkerUpdate = 10 * time.Millisecond
 
 // errNotEnoughWorkers is returned if the working set does not have enough
 // workers to successfully complete the download
