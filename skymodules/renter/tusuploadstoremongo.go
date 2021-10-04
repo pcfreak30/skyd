17e2e6d3fbef253db306e1669520b72dcf94be98
Chris Schinnerl <chris@siasky.net>
2021-10-04 10:02:35 +0200

    fix building indexes

--- skymodules/renter/tusuploadstoremongo.go
+++ skymodules/renter/tusuploadstoremongo.go
@@ -434,19 +434,6 @@ func newSkynetTUSMongoUploadStore(ctx context.Context, uri, portalName string, c
 		return nil, err
 	}
 
-	// Create the indices for the uploads collection.
-	indexes := []mongo.IndexModel{
-		{Keys: bson.M{"complete": 1}},
-		{Keys: bson.M{"lastwrite": 1}},
-		{Keys: bson.M{"servernames": 1}},
-	}
-	for _, idx := range indexes {
-		_, err = client.Database(TusDBName).Collection(TusUploadsMongoCollectionName).Indexes().CreateOne(ctx, idx)
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	// Sanity check portal name.
 	if portalName == "" {
 		return nil, errors.New("portalName can't be empty string")
@@ -456,6 +443,17 @@ func newSkynetTUSMongoUploadStore(ctx context.Context, uri, portalName string, c
 	us := &skynetTUSMongoUploadStore{
 		staticClient:         client,
 		staticPortalHostname: portalName,
+	}
+
+	// Create the indices for the uploads collection.
+	indexes := []mongo.IndexModel{
+		{Keys: bson.M{"complete": 1}},
+		{Keys: bson.M{"lastwrite": 1}},
+		{Keys: bson.M{"servernames": 1}},
+	}
+	_, err = us.staticUploadCollection().Indexes().CreateMany(ctx, indexes)
+	if err != nil {
+		return nil, err
 	}
 
 	// Create lock client.
