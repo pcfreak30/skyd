02e44008f749df770bf20cd8d71147a299b76dc9
Chris Schinnerl <chris@siasky.net>
2021-10-13 16:40:09 +0200

    better allocations in parseRootsFromData

--- skymodules/renter/proto/consts.go
+++ skymodules/renter/proto/consts.go
@@ -27,7 +27,7 @@ const (
 
 	// rootsDiskLoadBulkSize is the max number of roots we read from disk at
 	// once to avoid using up all the ram.
-	rootsDiskLoadBulkSize = 1024 * crypto.HashSize // 32 kib
+	rootsDiskLoadBulkSize = 1 << 16 * crypto.HashSize // 2 MiB
 
 	// remainingFile is a constant used to indicate that a fileSection can access
 	// the whole remaining file instead of being bound to a certain end offset.
